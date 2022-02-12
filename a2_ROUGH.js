5661793855185dba39bad5524fdc8c28f0dc82b6
studentN1 <91213428+studentN1@users.noreply.github.com>
2022-02-11 21:58:37 -0500

    a2 rough and finished

--- a2_ROUGH.js
+++ a2_ROUGH.js
@@ -10,15 +10,23 @@
 // Create an array of objects called courses. Each course in the array should have the 
 
 //define and create an object
+//const course = {code: "APC100", courseName: "Applied professional communication ", 
+//    hours: 3, url: "http:/www.senecacollege.ca"};
+
+//var APC100 = ["APC100", "Applied professional communication", 3, "http:/www.senecacollege.ca"];
 const APC100 = {code: "APC100", courseName: "Applied professional communication", hours: 3, url: "http:/www.senecacollege.ca"};
+//var IPC144 = ["IPC144", "Introduction to C programming", null, "http:/www.senecacollege.ca/ipc"];
 const IPC144 = {code: "IPC144", courseName: "Introduction to C programming", hours: null, url: "http:/www.senecacollege.ca/ipc"};
+//var ULI101 = ["ULI101", "Linux and the Internet", 4, "http:/www.senecacollege.ca/lin"];
 const ULI101 = {code: "ULI101", courseName: "Linux and the Internet", hours: 4, url: "http:/www.senecacollege.ca/lin"};
+//var IOS101 = ["IOS110", "Windows Operating System", 4, "http:/www.senecacollege.ca/ios"];
 const IOS101 = {code: "IOS110", courseName: "Windows Operating System", hours: 4, url: "http:/www.senecacollege.ca/ios"};
+//var EAC150 = ["EAC150", "College English", 3, null ];
 const EAC150 = {code: "EAC150", courseName: "College English", hours: 3, url: null };
 
 var courses = [APC100, IPC144, ULI101, IOS101, EAC150]; // courses array of objects (no longer an array of arrays)
 
-// test: checks array vv
+// checks array
 /*
 for (var i = 0; i < 5; i++) {
     console.log(i + ": " + courses[i].code);
@@ -51,13 +59,13 @@ console.log("Total hours for all courses: ", totalHours, "hours.");
 
 
 
-
-
-
-/***************************** Task2 *************************/
 console.log("\n");
 
+/***************************** Task2 *************************/
+
 // task2: a) Create a constructor function (named: Student) which will be used to create objects
+// https://www.w3schools.com/js/js_object_constructors.asp
+
 function Student(stuName, dob, sid, prgrm, gpa) {
     this.studentName = stuName;
     this.dateBirth = dob;
@@ -66,11 +74,25 @@ function Student(stuName, dob, sid, prgrm, gpa) {
     this.stuGPA = gpa;
 }
 
-var date0 = new Date(2002,2, 4); 
-const name0 = new Student("name0", date0, 1001, "BSD", 3.4);
+//var today = new Date();
+var date0 = new Date(2002,2, 4); // why is the month off???? same in tut tho??
 
-Student.prototype.toString = function studentToString() 
-{
+//toLocaleString();
+
+//const myFather = new Student("John", "3/4/2002", 50, "blue");
+const myFather = new Student("name0", date0, 1001, "BSD", 3.4);
+
+// https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Object/toString
+Student.prototype.toString = function studentToString() {
+    
+    /*
+    console.log("Student info for name0:\n" + 
+                "               Date of Birth: " + this.dateBirth.toLocaleString() + ",\n" + 
+                "               Student ID: " + this.stuID + ",\n" + 
+                "               Program: " + this.program + ",\n" +
+                "               GPA: " + this.stuGPA);  
+*/
+    //return `${this.studentName}`;
     return `${"Student info for " + this.studentName + ":\n" + 
     "               Date of Birth: " + this.dateBirth.toLocaleString() + ",\n" + 
     "               Student ID: " + this.stuID + ",\n" + 
@@ -79,7 +101,12 @@ Student.prototype.toString = function studentToString()
 };
 console.log(myFather.toString());
 
+
+// Student.prototype.toString();
+
+
 // task2: b) Create an empty array named students.
+
 var students = []; // empty array
 
 var myBday = new Date(1996,2,12);
@@ -98,23 +125,41 @@ students.push(chantelle);
 students.push(ben);
 
 // task2: c) Use the forEach method to iterate the array students and output to the console. 
+
+//students.forEach(element => console.log(element));
 students.forEach(element => console.log(element.toString()));
 
-
 //  task2: d) Create a separate function (name it highGPA())
+
 function highGPA(arrofStus)
 {
+    //var highestGPA = {}; // equivalent to: var person4 = new Object();
+    //var highestGPA = Object.create(Student);
+    //var highestGPA = Object.create(Student);
+
     var highestGPA = 0;
     var indexOfHighestGPA;
 
+    //arrofStus.forEach(element => console.log(element.toString()));
+
     for (let i = 0; i < arrofStus.length; i++) 
     {
-        // testing: values vv
-        // console.log("arrofstus gpa: "+arrofStus[i].stuGPA); 
-        // console.log("highest gpa: "+highestGPA);
 
+        console.log("arrofstus gpa: "+arrofStus[i].stuGPA);
+        //console.log("highest gpa: "+highestGPA.stuGPA);
+        console.log("highest gpa: "+highestGPA);
+
+        //if (arrofStus[i].stuGPA > highestGPA.stuGPA)
         if (arrofStus[i].stuGPA > highestGPA)
         {
+            /*
+            highestGPA.stuName = arrofStus[i].stuName;
+            highestGPA.dateBirth = arrofStus[i].dateBirth;
+            highestGPA.stuID = arrofStus[i].stuID;
+            highestGPA.program = arrofStus[i].program;
+            highestGPA.stuGPA = arrofStus[i].stuGPA;
+            console.log(highestGPA.toString());
+            */
             highestGPA = arrofStus[i].stuGPA;
             indexOfHighestGPA = i;
         }
