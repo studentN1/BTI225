107da09ab1b95c4b55fada08697c88ab4b6f9c05
studentN1 <91213428+studentN1@users.noreply.github.com>
2022-04-20 16:08:07 -0400

    a

---
+++ final/js/final.js
@@ -0,0 +1,10 @@
+var courseList=[
+    {courseCode:"BTP100", courseName:"Programming Fundamentals Using C",
+    courseOutline:"https://ict.senecacollege.ca/course/btp100"},
+    {courseCode:"BTI225", courseName:"Web Programming Principles",
+    courseOutline:"https://ict.senecacollege.ca/course/bti225"},
+    {courseCode:"BTI325", courseName:"Web Programming Tools and Frameworks",
+    courseOutline:"https://ict.senecacollege.ca/course/bti325"},
+    {courseCode:"BTI425", courseName:"Web Programming for Apps and Services",
+    courseOutline:"https://ict.senecacollege.ca/course/bti425"}
+];
